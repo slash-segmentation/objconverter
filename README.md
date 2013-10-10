167c16136fd691b3e056a94a64344469b610911c
coderforlife <jeff@coderforlife.com>
2013-10-10 13:42:57 -0700

    Added main README, updated source README, and cleaned up other code.

---
+++ README.md
@@ -0,0 +1,22 @@
+Program
+=======
+This program is based on the open-source
+[webgl-loader](http://code.google.com/p/webgl-loader/) project by Won
+Chun. It is based on r82 with many little additions to the converter. The
+output should be completely compatible with the original program. The
+objnormalize program was written by myself, and its functionality is now
+(in part) integrated into the original webgl-loader.
+
+
+Samples
+-------
+The samples directory contains a _very_ basic WebGL viewer for the
+produced models. It has problems with scale sometimes and does not show
+the backs of surfaces.
+
+
+Online Version
+--------------
+You can go to
+[webgl-tests.crbs.ucsd.edu/obj-viewer/](http://webgl-tests.crbs.ucsd.edu/obj-viewer/)
+for an online-usable version of the program.
--- src/objnormalize.cc
+++ src/objnormalize.cc
@@ -1,3 +1,21 @@
+#if 0  // A cute trick to making this .cc self-building from shell.
+g++ $0 -O2 -Wall -Werror -o `basename $0 .cc`;
+exit;
+#endif
+// Copyright 2011 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License. You
+// may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdlib.h>
